ad1c92972f01b2f2840f102e2a5c2657fce5b89e
Yini <ydxb7@mail.missouri.edu>
2019-05-24 16:36:25 -0700

    init

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.pets.data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * API Contract for the Pets app.
+ */
+public final class PetContract {
+    // Content Authority:
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    // create base content URI: scheme + CONTENT_AUTHORITY
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    // 此常数存储位置将附加到基本内容 URI 的每个表的路径。
+    public static final String PATH_PETS = "pets";
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private PetContract() {
+    }
+
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
+    public static final class PetEntry implements BaseColumns {
+        //在 contract 中的每个 Entry 类中，我们为类创建一个完整的 URI 作为常数，称为 CONTENT_URI。
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
+        /**
+         * Name of database table for pets
+         */
+        public final static String TABLE_NAME = "pets";
+
+
+        /**
+         * Unique ID number for the pet (only for use in the database table).
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the pet.
+         * <p>
+         * Type: TEXT
+         */
+        public final static String COLUMN_PET_NAME = "name";
+
+        /**
+         * Breed of the pet.
+         * <p>
+         * Type: TEXT
+         */
+        public final static String COLUMN_PET_BREED = "breed";
+
+        /**
+         * Gender of the pet.
+         * <p>
+         * The only possible values are {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
+         * or {@link #GENDER_FEMALE}.
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PET_GENDER = "gender";
+
+        /**
+         * Weight of the pet.
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * Possible values for the gender of the pet.
+         */
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+
+}
+
